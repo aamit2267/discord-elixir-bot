a5a4d14683fad1a4e93802471a97831eb5d5b627
Amit Agarwal <60168762+aamit2267@users.noreply.github.com>
2021-01-15 13:24:42 +0530

    Update thonk.ex

--- lib/thonk.ex
+++ lib/thonk.ex
@@ -2,6 +2,17 @@ defmodule Thonk do
   use Application
   alias Alchemy.{Client, Cogs}
   
+  def start(_type, _args) do
+    case Application.get_env(:thonk, :token) do
+      nil ->
+        raise "TOKEN environment variable is not set"
+
+      token ->
+        prefix = Application.fetch_env!(:thonk, :prefix)
+        bootstrap(token, prefix)
+    end
+  end
+  
   defp bootstrap(token, prefix) do
     run = Client.start(token)
     load_modules()
