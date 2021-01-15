4cd0aa0f91516f42ab281caad45bbb1321d46117
Amit Agarwal <60168762+aamit2267@users.noreply.github.com>
2021-01-15 13:24:03 +0530

    Update thonk.ex

--- lib/thonk.ex
+++ lib/thonk.ex
@@ -12,16 +12,4 @@ defmodule Thonk do
         bootstrap(token, prefix)
     end
   end
-
-  defp bootstrap(token, prefix) do
-    run = Client.start(token)
-    load_modules()
-    Cogs.set_prefix(prefix)
-    run
-  end
-
-  defp load_modules do
-    use Thonk.Events
-    use Thonk.Commands.Basic
-  end
 end
